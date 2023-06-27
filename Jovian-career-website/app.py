8677f6fc6de2515e11d2a05eba07bc67806ea81c
anandvarrier <46650077+anandvarrier@users.noreply.github.com>
2023-06-27 12:33:07 +0000

    #1 Modification

--- Jovian-career-website/app.py
+++ Jovian-career-website/app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def helloworld():
-  return 'Hello Replit flask app created by Anand.'
+  return render_template('home.html')
 
 
 print(__name__)
