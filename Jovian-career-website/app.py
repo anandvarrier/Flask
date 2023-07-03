a689537a316f18ce35e22084a65894178da7b093
anandvarrier <46650077+anandvarrier@users.noreply.github.com>
2023-07-03 13:43:00 +0000

    #3 Modification

--- Jovian-career-website/app.py
+++ Jovian-career-website/app.py
@@ -2,12 +2,56 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': '001',
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary': 'Rs. 10,00,000/-'
+  },
+  {
+    'id': '002',
+    'title': 'Business Analyst',
+    'location': 'Bengaluru, India',
+    'salary': 'Rs. 13,000,000/-'
+  },
+  {
+    'id': '003',
+    'title': 'Data Scientist',
+    'location': 'Mumbai, India',
+    'salary': 'Rs. 2,000,000/-'
+  },
+  {
+    'id': '004',
+    'title': 'Financial Analyst',
+    'location': 'Bengaluru, India',
+    'salary': 'Rs. 4,000,000/-'
+  },
+  {
+    'id': '005',
+    'title': 'Marketing Analyst',
+    'location': 'Pune, India',
+    'salary': 'Rs. 5,000,000/-'
+  },
+  {
+    'id': '006',
+    'title': 'Backend Engineer',
+    'location': 'Remote',
+    'salary': 'Rs. 5,000,000/-'
+  },
+  {
+    'id': '007',
+    'title': 'Python developer',
+    'location': 'Pune, India',
+    'salary': 'Rs. 4,000,000'
+  },
+]
+
 
 @app.route("/")
-def helloworld():
-  return render_template('home.html')
+def hellojovian():
+  return render_template('home.html', jobs=JOBS, company_name="Jovian")
 
 
-print(__name__)
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
